51f7dc8db1ad8d381e500c88be69b9708ae1e1b8
Sazuna <liza.fretel@gmail.com>
2023-12-23 16:43:43 +0100

    Ajout du numéro du token dans le message d'erreur.

--- tokenize_MTdVplus.py
+++ tokenize_MTdVplus.py
@@ -70,6 +70,7 @@ def make_tokens(lines: List[str], lines_number: List[int]):
 					erreur("Token non autorisé", token=token, line=line, line_n=line_n)
 				instruction_n += 1
 				tokens.append([line_n, token, type_token, instruction_n, type_instruction, '-']) # le '-' est la position operation.
+	print("\033[92mFichier TSplus valide.")
 	return tokens
 
 def check_test(expression, line_n, instruction_n):
@@ -80,7 +81,7 @@ def check_test(expression, line_n, instruction_n):
 	returns: un tableau de tokens, avec pour chaque token les informations [line_n, token, type_token, instruction_n, type_instruction, position_operation], dans cet ordre-là.
 	"""
 	variable_regex = r"([a-z]|[A-Z]|_)([a-z]|[A-Z]|[0-9]|_)*"
-	chiffre_regex = r"[0-9]+" # TODO il faudra vérifier que c'est entre 0 et 29
+	chiffre_regex = r"[0-9]+"
 	egalite_regex = r"=="
 	si_regex = r"si"
 	operateur_regex = r"[\+\*]"
@@ -88,11 +89,11 @@ def check_test(expression, line_n, instruction_n):
 
 	# Vérification de la structure externe du test : si ( ... )
 	if tokens[0] != "si":
-		erreur("Un test doit commencer par le mot-clé si.\n\t syntaxe: si (operation1 test operation2)", token=tokens[0], line_n=line_n, line=expression)
+		erreur("Un test doit commencer par le mot-clé si.\n\t syntaxe: si (operation1 test operation2)", token=tokens[0], line_n=line_n, line=expression, token_n=1)
 	elif tokens[1] != "(":
-		erreur("Un test doit être entouré par des parenthèses.\n\t syntaxe: si (operation1 test operation2)", token=tokens[1], line_n=line_n, line=expression)
+		erreur("Un test doit être entouré par des parenthèses.\n\t syntaxe: si (operation1 test operation2)", token=tokens[1], line_n=line_n, line=expression, token_n=2)
 	elif tokens[-1] != ")":
-		erreur("Un test doit être entouré par des parenthèses.\n\t syntaxe: si (operation1 test operation2)", token=tokens[-1], line_n=line_n, line=expression)
+		erreur("Un test doit être entouré par des parenthèses.\n\t syntaxe: si (operation1 test operation2)", token=tokens[-1], line_n=line_n, line=expression, token_n=len(tokens))
 	position = 'G'
 	operations_g_d = {'G': [[],[]], 'D': [[],[]]} # ['G'][0] => les tokens de gauche. ['D'][0] => les tokens de droite.
 	# ['G'][1] => les types des tokens de gauche. ['D'][1] => les types des tokens de droite.
@@ -100,9 +101,9 @@ def check_test(expression, line_n, instruction_n):
 	# Vérification des tokens
 	# et création de la liste de sortie
 	sortie = []
-	for token in tokens:
+	for i, token in enumerate(tokens):
 		if token in ("boucle", "D", "G", '#', '}', 'fin', 'I', 'P', '='):
-			erreur("Mot-clé non autorisé dans un test.", token=token, line_n=line_n, line=expression)
+			erreur("Mot-clé non autorisé dans un test.", token=token, line_n=line_n, line=expression, token_n=i+1)
 		elif regex.match(si_regex, token):
 			sortie.append([line_n, token, "complexe", instruction_n, "test", '-'])
 		elif regex.match(variable_regex, token):
@@ -115,7 +116,7 @@ def check_test(expression, line_n, instruction_n):
 			operations_g_d[position][1].append("valeur")
 		elif regex.match(egalite_regex, token):
 			if (position == 'D'):
-				erreur("Un test d'égalité ne peut pas contenir deux fois l'opérateur ==.", token=token, line_n=line_n, line=expression)
+				erreur("Un test d'égalité ne peut pas contenir deux fois l'opérateur ==.", token=token, line_n=line_n, line=expression, token_n=i+2)
 			sortie.append([line_n, token, "operateur", instruction_n, "test", 'M'])
 			position = 'D'
 		elif regex.match(operateur_regex, token):
@@ -123,9 +124,9 @@ def check_test(expression, line_n, instruction_n):
 			operations_g_d[position][0].append(token)
 			operations_g_d[position][1].append("operateur")
 		elif token not in ['(', ')']:
-			erreur("Token non autorisé dans un test.", token=token, line_n=line_n, line=expression)
-	check_operation(operations_g_d["G"][0], operations_g_d["G"][1], line_n, expression)
-	check_operation(operations_g_d["D"][0], operations_g_d["D"][1], line_n, expression)
+			erreur("Token non autorisé dans un test.", token=token, line_n=line_n, line=expression, token_n=i+1)
+	check_operation(operations_g_d["G"][0], operations_g_d["G"][1], line_n, expression, 1)
+	check_operation(operations_g_d["D"][0], operations_g_d["D"][1], line_n, expression, len(operatopns_g_d["G"]) + 1)
 	return sortie
 
 def check_affectation(expression, line_n, instruction_n):
@@ -148,14 +149,14 @@ def check_affectation(expression, line_n, instruction_n):
 
 	# Vérifier qu'on a bien une variable à gauche
 	if left_token  in ("boucle", "si", "D", "G", 'fin', 'I', 'P'):
-		erreur("Côté gauche de l'affectation ne peut pas être un mot-clé.", token=left_token, line_n=line_n, line=expression)
+		erreur("Côté gauche de l'affectation ne peut pas être un mot-clé.", token=left_token, line_n=line_n, line=expression, token_n=0)
 
 	if not regex.match(variable_regex, left_token):
-		erreur("Côté gauche de l'affectation doit être une variable.", token=left_token, line_n=line_n, line=expression)
+		erreur("Côté gauche de l'affectation doit être une variable.", token=left_token, line_n=line_n, line=expression, token_n=0)
 
 	# Vérifier qu'on a bien une affectation
 	if tokens[1] != affectation_regex:
-		erreur("Une affectation doit avoir un seul symbole à gauche du =.", token=tokens[1], line_n=line_n, line=expression)
+		erreur("Une affectation doit avoir un seul symbole à gauche du =.", token=tokens[1], line_n=line_n, line=expression, token_n=1)
 
 	types = []	
 
@@ -191,30 +192,28 @@ def check_affectation(expression, line_n, instruction_n):
 				types.append(type_token)
 				if token  in ("boucle", "si", "D", "G", 'fin', 'I', 'P'):
 					# Si le token est un mot clé, c'est une erreur
-					erreur("Côté droite de l'affectation ne peut pas contenir un mot-clé.", token=token, line_n=line_n, line=expression)
+					erreur("Côté droite de l'affectation ne peut pas contenir un mot-clé.", token=token, line_n=line_n, line=expression, token_n=i)
 			else:
-				erreur("Token non autorisé dans une affectation.", token, line_n, expression)
+				erreur("Token non autorisé dans une affectation.", token, line_n, expression, token_n=i)
 		# Ajouter le token et ses informations à la liste des résultats
 		resultat.append([line_n, token, type_token, instruction_n, "affectation", position_operation])
 
-	check_operation(right_tokens, types, line_n, expression)
+	check_operation(right_tokens, types, line_n, expression, 2)
 		
 
 	return resultat
 
-#TODO
 """
 Les paramètres line_n et line servent à l'affichage des erreurs.
 
 returns: None
 """
-def check_operation(tokens, types, line_n, line):
+def check_operation(tokens, types, line_n, line, len_left=0):
 	"""
 	tokens: liste des tokens de l'opération.
 	types: type des tokens de l'opération.
 	Fonction qui ne fait rien si l'opération est valide.
 	Sinon, elle affiche une erreur avec la fonction erreur.
-	TODO
 	Vérifie qu'on a bien une suite de types qui fasse v o v o v o v, et que les o sont soit * soit	avec: v = variable ou valeur, o = operateur.
 
 	par exemple
@@ -235,25 +234,26 @@ def check_operation(tokens, types, line_n, line):
 		# affichage de l'erreur d'opération:
 		# erreur("Opération invalide", token=token, line_n = line_n, line = line)
 		i += 1
+		if typen not in ("operateur", "valeur", "variable"):
+			erreur("Token inattendu dans une opération.", token, line_n=line_n, line=line, token_n=i + len_left)
 		if i % 2 == 0:
 			if typen != "operateur":
-				erreur("Opérateur attendu (+ ou *) dans l'opération.\nOpération binaire incomplète.", token, line_n=line_n, line=line)
+				erreur("Opérateur attendu (+ ou *) dans l'opération\nOpération binaire incomplète.", token, line_n=line_n, line=line, token_n=i + len_left)
 			elif not regex.search(operateur_regex, token):
 				# Si l'opérateur n'est pas un opérateur valide (pas d'opérateur d'affectation ou de test)
-				erreur("Opérateur invalide dans une opération.", token, line_n=line_n, line=line)
+				erreur("Opérateur invalide dans une opération.", token, line_n=line_n, line=line, token_n=i+len_left)
 		elif i % 2 != 0:
 			if typen != "variable" and typen != "valeur":
-				erreur("Valeur ou variable attendue dans l'opération.\nOpération binaire incomplète.", token, line_n=line_n, line=line)
-				# TODO afficher un message plus précis
+				erreur("Valeur ou variable attendue dans l'opération.\nOpération binaire incomplète.", token, line_n=line_n, line=line, token_n=i+len_left)
 			elif typen == "valeur":
 				if int(token) < 0 or int(token) > 29:
-					erreur("Entier dépassant la limite autorisée.\n\tValeurs autorisées: [0-29]", token=token, line_n=line_n, line=line)
+					erreur("Entier dépassant la limite autorisée.\n\tValeurs autorisées: [0-29]", token=token, line_n=line_n, line=line, token_n=i+len_left)
 	if i % 2 == 0:
-		# en sortant de la boucle, il faut que i est bien un chiffre impair
+		# en sortant de la boucle, il faut que i soit bien un chiffre impair
 		# sinon il manque sans doute un 'v' final.
 		erreur("Une opération ne peut pas se terminer par un opérateur.", tokens[-1], line_n = line_n, line = line)
 
-def erreur(erreur_str, token="", line_n="", line=""):
+def erreur(erreur_str, token="", line_n="", line="", token_n=""):
 	print("\033[91m", end="") # Met les prochains prints en rouge
 	print(erreur_str)
 	if line_n or line or token:
@@ -264,6 +264,8 @@ def erreur(erreur_str, token="", line_n="", line=""):
 		print("\t" + line)
 	if token:
 		print("sur le token '" + str(token) + "'")
+	if token_n:
+		print("à la position "+str(token_n) + ".")
 	print("\033[0m", end="") # Met les prochains prints en jaune
 	sys.exit(1)
 
