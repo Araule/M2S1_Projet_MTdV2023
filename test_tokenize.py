15e6b5bf502c9d47952b0a60871f2fc82a0a8b9c
Sazuna <liza.fretel@gmail.com>
2023-12-23 16:42:08 +0100

    Suppression de fichiers en trop

--- test_tokenize.py
+++
@@ -1,6 +0,0 @@
-import tokenize
-
-# 3 : n° ligne de l'expression
-# expression complète :  "y = x * 3"
-
-tokenize.check_operation(['_kenza', '+', '3', '*', 'liza'], ['variable', 'operateur', 'valeur', 'operateur', 'variable'], 1, "y = _kenza +")
